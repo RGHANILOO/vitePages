a935af2ef3d2f8d3cd76f2909c4dfb895a8daad6
“rezan5910” <bytemefirst@gmail.com>
2024-11-28 03:07:05 +0000

    update workflows pnpm job

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return <>
-  <h1>Welcome to the Home Page</h1>
+  <h1 color="darkpurple">Welcome to the Home Page</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, explicabo!</p>
   </>;
 };
